a4eff4e205343de52a187f8fe4bc03673d7fcd52
Hunter N <hunter@Hunters-MacBook-Pro.local>
2024-03-30 09:50:34 -0400

    fixing animations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 import netlify from "@astrojs/netlify";
+import { defineConfig } from "astro/config";
+import lottie from "astro-integration-lottie";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(),lottie(),],
   output: "server",
   adapter: netlify()
 });
